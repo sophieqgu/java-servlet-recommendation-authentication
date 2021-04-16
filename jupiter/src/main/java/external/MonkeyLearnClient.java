cc67c30c799e6d6c1646956ebfac012667b51176
Sophie Gu <sophieqgu@gmail.com>
2021-04-16 11:47:28 -0400

    Config MonkeyLearnClient

---
+++ jupiter/src/main/java/external/MonkeyLearnClient.java
@@ -0,0 +1,58 @@
+package external;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.monkeylearn.ExtraParam;
+import com.monkeylearn.MonkeyLearn;
+import com.monkeylearn.MonkeyLearnException;
+import com.monkeylearn.MonkeyLearnResponse;
+
+
+public class MonkeyLearnClient {
+	
+	
+	private static final String API_KEY = api_key;
+
+	public static List<List<String>> extractKeywords(String[] text) {
+		if (text == null || text.length == 0) {
+			return new ArrayList<>();
+		}
+
+		MonkeyLearn ml = new MonkeyLearn(API_KEY);
+
+		// Use the keyword extractor
+		ExtraParam[] extraParams = { new ExtraParam("max_keywords", "3") };
+		MonkeyLearnResponse response;
+		try {
+			response = ml.extractors.extract("ex_YCya9nrn", text, extraParams);
+			JSONArray resultArray = response.arrayResult;
+			return getKeywords(resultArray);
+		} catch (MonkeyLearnException e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<>();
+	}
+
+	private static List<List<String>> getKeywords(JSONArray mlResultArray) {
+		List<List<String>> topKeywords = new ArrayList<>();
+		
+		for (int i = 0; i < mlResultArray.size(); ++i) {
+			List<String> keywords = new ArrayList<>();
+			JSONArray keywordsArray = (JSONArray) mlResultArray.get(i);
+			for (int j = 0; j < keywordsArray.size(); ++j) {
+				JSONObject keywordObject = (JSONObject) keywordsArray.get(j);
+			
+				String keyword = (String) keywordObject.get("keyword");
+				keywords.add(keyword);
+
+			}
+			topKeywords.add(keywords);
+		}
+		return topKeywords;
+	}
+
+}
